d93240ddcf1833929429d9da68f7f9f03970e8d9
francky mancini <stillin33@hotmail.com>
2016-04-11 03:19:59 +0200

    new design for index.php

--- php/index.php
+++ php/index.php
@@ -1,49 +1,30 @@
 ﻿<!DOCTYPE html>
 <html>
-<head>
-<!--[if lt IE 9]>
-<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
-<![endif] -->
-<title> MYSTERES ET ILLUSIONS </title>
- <meta charset="utf-8"/>
- <meta name="AUTHOR" content="Franck M. CNAM Bourges">
- <meta name="Description" content="site scientifique sur des mysteres ou illusions d'optique, découvrez les stéréogrammes, le phénomène des mirages et des images subliminales" />	
- <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
- <link rel="stylesheet" href ="../css/standard.css"/>
- <link rel="shortcut icon" href="../images/favicon.ico" />
-</head>
+  <head>
+	<!--[if lt IE 9]>
+	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif] -->
+	<title> MYSTERES ET ILLUSIONS </title>
+	<meta charset="utf-8"/>
+	<meta name="AUTHOR" content="Franck M. CNAM Bourges">
+	<meta name="Description" content="site scientifique sur des mysteres ou illusions d'optique, découvrez les stéréogrammes, le phénomène des mirages et des images subliminales" />	
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=yes" />
+	<link rel="stylesheet" href ="../css/standard.css"/>
+	<link rel="shortcut icon" href="../images/favicon.ico" />
+  </head>
 	
 <body>
+
 <div id="conteneur">
 
 <header>  
+<a href ="./index.php" title="retour à l'accueil"> <img src="../images/cube_fou.jpg" id="logo"  ></a>
 <?php
  include ("inc/header.inc.php"); headerinclude ("accueil principal");
- include ("inc/cookie.inc.php"); cookieinclude ("accueilprincipal");
+ include ("inc/banniere.inc.php"); 
 ?>
-
-<!-- banniere defilante en html -->
-<marquee scrollamount="5" direction="left" onmouseover="this.stop()" onmouseout="this.start()">&nbsp;
-<IMG src="../images/mysteres/swnewyork.jpg" height="200px">
-<IMG src="../images/mysteres/rocher.jpg"height="200px">
-<IMG src="../images/mysteres/boule.jpg"height="200px">
-</MARQUEE>
-
-
-</header>
-
-
-<nav>
-<a href ="./index.php"> <img src="../images/cube_fou.jpg" width="20%" align="left" ></a>
-<ul id="menu">
-<?php  
- include ("./inc/menu.inc.php"); 
- //include ("./inc/menu.inc.php"); menuinclude ("mystillus.esy.es"); 
- ?>
-  </ul>
-</nav>
-
 <div id="block-user-login" >
+<h1>Espace Membre</h1>
   <form name="formulaire" method="GET" action="./login.php" onSubmit="return verifForm(this);" >
     <label>Login : (*)</label>
     <input type="text" id="login" name="login" placeholder=" sans symbole < " required onblur="LoginSansInf(this);" > <span id="erreur1"></span> <br /><br>
@@ -51,9 +32,21 @@
     <input id="envoi" name="envoi" type="submit"  value="Envoyer" /> <span id="erreur4"></span> 
     </form>
 </div>
+</header>
 
- <!-- <img id="imageenhaut" src="../images/mysteres/swnewyork.jpg" width="80%"  height="250px" align="center" ></a> -->
+<aside>
+<?php
+ include ("inc/cookie.inc.php"); cookieinclude ("accueilprincipal");
+?>
+</aside>
 
+<nav>
+<ul id="menu">
+<?php  
+ include ("./inc/menu.inc.php"); 
+ ?>
+  </ul>
+</nav>
 
 <section>	
 <article>
@@ -61,9 +54,9 @@
  <p>Ce site, développé par Franck M. du Cnam de Bourges, a pour but de vous faire partager divers mystères et illusions d'optique, et d'en expliquer le fonctionnement. Vous trouverez sur certains mots des informations info-bulles supplémentaires. Bonne navigation !</p> 
 </article>
 <article>
- <h2><a href="./html/mysteres.html">MYSTERES</a></h2>
+ <h2><a href="./html/mysteres.html" title="page des mystères" >MYSTERES</a></h2>
  <p>Présentation de divers mystères scientifiques, élucidés ou non, sous divers formats avec description et explications. </p>
- <h2><a href="./html/illusions.html">ILLUSIONS</a ></h2>
+ <h2><a href="./html/illusions.html" title="page des illusions">ILLUSIONS</a ></h2>
  <P> Présentation d'incroyables illusions d'optique, sous divers formats( vidéos, photos, ...) avec description et explications .</p>
 </article>
 </section>
@@ -76,8 +69,8 @@
 </footer>
 
 </div>
-  <!-- <script src="../js/AfficheOuMasquePassword.js"></script>
-  <script src="../js/VerifFormulaire.js"></script> -->
+  <script src="../js/AfficheOuMasquePassword.js"></script>
+  <script src="../js/VerifFormulaire.js"></script>
 
 </body>
 </html>
